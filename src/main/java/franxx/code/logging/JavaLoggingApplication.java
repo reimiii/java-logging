13410c626b5dc894cec3bbb2a02abc2fd12c9720
Hilmi AM <imiia75775@proton.me>
2023-12-21 16:45:14 +0700

    Initial commit

---
+++ src/main/java/franxx/code/logging/JavaLoggingApplication.java
@@ -0,0 +1,13 @@
+package franxx.code.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLoggingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/franxx/code/logging/JavaLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package franxx.code.logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
