a843c39d33c77c05aee3dc6b0eb708a515714860
Hilmi AM <imiia75775@proton.me>
2023-12-21 17:22:35 +0700

    Level log

---
+++ src/test/java/franxx/code/logging/LevelTest.java
@@ -0,0 +1,19 @@
+package franxx.code.logging;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LevelTest {
+
+    private static final Logger log = LoggerFactory.getLogger(LevelTest.class);
+
+    @Test
+    void level() {
+        log.trace("Trace");
+        log.debug("Debug");
+        log.info("Info");
+        log.warn("Waring");
+        log.error("Error");
+    }
+}
